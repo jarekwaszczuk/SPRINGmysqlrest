f88df1fc36eda071de637d13e6d147292f9ca087
Jarosław Waszczuk <jarekwaszczuk@poczta.fm>
2018-08-13 20:48:49 +0200

    Initial commit

---
+++ src/main/java/pl/jarek/mysqlrest/MysqlrestApplication.java
@@ -0,0 +1,12 @@
+package pl.jarek.mysqlrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlrestApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/jarek/mysqlrest/MysqlrestApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.jarek.mysqlrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysqlrestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
