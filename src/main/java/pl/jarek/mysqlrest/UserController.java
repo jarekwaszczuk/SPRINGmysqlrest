b33e8ae077c50e3e7683312b3b05bac82d6f3a6b
Jarosław Waszczuk <jarekwaszczuk@poczta.fm>
2018-08-14 18:01:16 +0200

    UserController

--- src/main/java/pl/jarek/mysqlrest/UserController.java
+++ src/main/java/pl/jarek/mysqlrest/UserController.java
@@ -1,13 +1,24 @@
 package pl.jarek.mysqlrest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController(value = "/users")
 public class UserController {
 
+    @Autowired
     private final UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> findAll(){
+        return new ArrayList<>();
     }
 }
